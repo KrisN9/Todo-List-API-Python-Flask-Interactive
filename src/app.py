28f2a0520b8942dacd78b4450d57b6111bebc705
KrisN9 <kristinank992@gmail.com>
2022-12-05 19:20:12 +0000

    Tutorial Todos terminado

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 import json
+
 app = Flask(__name__)
 
 todos = [
@@ -16,9 +17,16 @@ def get_todos():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
     print("Incoming request with the following body", request_body)
-    request.data = json.loads(todos)
-    return jsonify(request.data)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
